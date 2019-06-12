636df557407d9b226ff16c05a840edd2d467701a
esentio <devel@esentio.sk>
2019-06-12 20:48:13 +0200

    Nuget related changes

---
+++ Source/Nuget/pack.bat
@@ -0,0 +1,3 @@
+dotnet pack "../Source/Yamo/Yamo.vbproj" -c Release -o "../../../Setup"
+dotnet pack "../Source/Yamo.SQLite/Yamo.SQLite.vbproj" -c Release -o "../../../Setup"
+dotnet pack "../Source/Yamo.SqlServer/Yamo.SqlServer.vbproj" -c Release -o "../../../Setup"
---
+++ Source/Nuget/push.bat
@@ -0,0 +1,7 @@
+@echo off
+
+set /p key="Enter API key: "
+
+dotnet nuget push "../../Setup/Yamo.0.4.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
+dotnet nuget push "../../Setup/Yamo.SQLite.0.4.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
+dotnet nuget push "../../Setup/Yamo.SqlServer.0.4.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
