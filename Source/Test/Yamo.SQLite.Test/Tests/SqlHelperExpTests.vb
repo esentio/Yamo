47c23006a58ac9e5e30cd0695f505d15b2df93ab
esentio <devel@esentio.sk>
2020-07-13 16:43:31 +0200

    Added comments

--- Source/Test/Yamo.SQLite.Test/Tests/SqlHelperExpTests.vb
+++ Source/Test/Yamo.SQLite.Test/Tests/SqlHelperExpTests.vb
@@ -14,6 +14,7 @@ Namespace Tests
     Public Overrides Sub SelectUsingIIf()
       ' do nothing
       ' TODO: work since SQLite 3.32.0, which was released on 22 May 2020 - update SQLite and test this!
+      ' For support of SQLite 3.32.0 see: https://github.com/ericsink/SQLitePCL.raw/issues/350
     End Sub
 
   End Class
