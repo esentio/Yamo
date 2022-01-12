782b9e0bdc52ee02aa92ce4210b1770dbe9b4fef
esentio <devel@esentio.sk>
2022-01-12 20:28:23 +0100

    Updated samples in C# playground project

---
+++ Source/Samples/Yamo.Playground.CS/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yamo.Playground.CS.Model
+{
+    class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
