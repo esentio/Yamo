d22af891c4dda5ca5ef677c1b7c0ce3cb678cd76
esentio <devel@esentio.sk>
2022-01-29 10:25:06 +0100

    Version raised to 0.8.0

--- Source/Nuget/push.bat
+++ Source/Nuget/push.bat
@@ -2,6 +2,6 @@
 
 set /p key="Enter API key: "
 
-dotnet nuget push "../../Setup/Yamo.0.7.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
-dotnet nuget push "../../Setup/Yamo.SQLite.0.7.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
-dotnet nuget push "../../Setup/Yamo.SqlServer.0.7.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
+dotnet nuget push "../../Setup/Yamo.0.8.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
+dotnet nuget push "../../Setup/Yamo.SQLite.0.8.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
+dotnet nuget push "../../Setup/Yamo.SqlServer.0.8.0.nupkg" -k %key% -s https://api.nuget.org/v3/index.json
